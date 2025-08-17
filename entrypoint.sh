658bee596f26d39f5a72ac002abc1507b65c11fd
Решетинский Дмитрий <Dimantchick@gmail.com>
2025-08-17 21:38:34 +0700

    Schedule from environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Проверяем наличие переменной CRON_SCHEDULE и используем значение по умолчанию
+CRON_SCHEDULE="${CRON_SCHEDULE:-0 2 * * *}"
+
 # Создаем crontab
-echo "0 2 * * * /app/pg_backup.sh >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
+echo "$CRON_SCHEDULE /app/pg_backup.sh >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
 echo "*/5 * * * * date >> /proc/1/fd/1" >> /etc/crontabs/root
 chmod 0644 /etc/crontabs/root
 
